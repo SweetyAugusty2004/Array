d34dc529000631e9677d19584e7d59314f9c0f25
SweetyAugusty2004 <sweetyaugusty2004@gmail.com>
2024-07-10 20:29:25 +0530

    Add files via upload

---
+++ array/app.js
@@ -0,0 +1,40 @@
+
+let fruits = ['Apple', 'Banana', 'Cherry'];
+    fruits.push('Grapes'); 
+    console.log(fruits);
+
+
+  let arr=[1,2,3,4,5];
+  let arr2=['computer', 'science'];
+  arr.push(10,20);
+  console.log(arr);
+  
+
+  arr.pop();
+  console.log(arr);
+
+
+  arr.unshift(0);
+  console.log(arr);
+  
+
+let fruit = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+citrus = fruit.slice(1);
+console.log(citrus);
+
+
+fruit.splice(2, 2, "Grapes", "Kiwi");
+console.log(fruit);
+
+
+let arr10 = ["Angel", "Sweety"];
+let arr20 = ["Augusty", "Imman"];
+let arr30 = ["student", "worker"];
+console.log(arr10, arr20, arr30);
+ let arr40 = arr10.concat(arr20, arr30);
+ console.log(arr40);
+
+ let fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+ console.log(fruits2);
+delete fruits2[0];
+console.log(fruits2);
